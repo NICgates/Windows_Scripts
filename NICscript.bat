742e0f703b63a453a43d374bf34d9257acdcc0e6
NICgates <39223425+NICgates@users.noreply.github.com>
2022-05-24 14:08:46 -0700

    Update NICscript.bat

--- NICscript.bat
+++ NICscript.bat
@@ -1,3 +1,5 @@
+::Collection of commands for managing unnecessary C drive space taken up by unnecessary programs and files. 
+
 :: Delete shadow copies & resize max storage for it.
 vssadmin delete shadows /for=C:
 vssadmin resize shadowstorage /for=C: /on=C: /maxsize=10GB
@@ -7,4 +9,4 @@ rmdir /s %systemdrive%\$Recycle.bin
 cleanmgr.exe /d c
 
 :: Uninstall programs and remove their files
-msiexec.exe /X {uninstall string} /qn
+:: msiexec.exe /X {uninstall string} /qn
