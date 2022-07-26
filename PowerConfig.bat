f6c882688b717a5013bedb3e80f9c6ba017a5a0e
NICgates <39223425+NICgates@users.noreply.github.com>
2022-07-26 12:50:30 -0700

    Update PowerConfig.bat

--- PowerConfig.bat
+++ PowerConfig.bat
@@ -9,3 +9,9 @@ powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 :: CsEnabled value data to 0
 REG DELETE HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power /v CsEnabled
 REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power /v CsEnabled /d 0
+
+:: --------------------------------------------------------------
+:: Allow Wake Timers
+powercfg /SETDCVALUEINDEX SCHEME_CURRENT 238c9fa8-0aad-41ed-83f4-97be242c8f20 bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d 1
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 238c9fa8-0aad-41ed-83f4-97be242c8f20 bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d 1
+
