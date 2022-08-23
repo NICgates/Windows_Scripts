2877bc8d8829e1182c5e1293d44b5087f73c617a
NICgates <39223425+NICgates@users.noreply.github.com>
2022-08-23 15:42:41 -0700

    Update uninstallTeams.ps1

--- uninstallTeams.ps1
+++ uninstallTeams.ps1
@@ -9,7 +9,7 @@ $userslist = Get-ChildItem -Directory "C:\Users"
 
 foreach ($userVar in $userslist) {
     if (Test-Path "C:\Users\$userVar\AppData\Local\Microsoft\Teams\Update.exe") {
-        Start-Process -FilePath "C\Users\$userVar\AppData\Local\Microsoft\Teams\Update.exe" -ArgumentList "-uninstall -s" -EA Stop
+        Start-Process -FilePath "C:\Users\$userVar\AppData\Local\Microsoft\Teams\Update.exe" -ArgumentList "-uninstall -s" -EA Stop
     }
 }
 
