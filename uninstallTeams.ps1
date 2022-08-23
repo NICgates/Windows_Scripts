d0cc66b5275380bbefc61e83abc66932a3912ad6
NICgates <39223425+NICgates@users.noreply.github.com>
2022-08-23 15:18:21 -0700

    Create uninstallTeams.ps1

---
+++ uninstallTeams.ps1
@@ -0,0 +1,16 @@
+#
+# Uninstall Teams 
+#
+
+
+Set-ExecutionPolicy RemoteSigned
+
+$userslist = Get-ChildItem -Directory "C:\Users"
+
+foreach ($userVar in $userslist) {
+    if (Test-Path "C:\Users\$userVar\AppData\Local\Microsoft\Teams\Update.exe") {
+        Start-Process -FilePath "C\Users\$($_.Name)\AppData\Local\Microsoft\Teams\Update.exe" -ArgumentList "-uninstall -s" -EA Stop
+    }
+}
+
+Set-ExecutionPolicy Restricted 
