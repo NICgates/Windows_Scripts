53c4a9d7158dd2d54b9393399f172d48709b172e
NICgates <39223425+NICgates@users.noreply.github.com>
2022-08-31 15:00:29 -0700

    Update checkProgram.ps1

--- checkProgram.ps1
+++ checkProgram.ps1
@@ -1,11 +1,24 @@
 
-# Set the software app to what you'd like to install or uninstall.
-$software = "APP";
+
+# Set the software to application to install or uninstall.
+$software = "Hard Disk Sentinel";
 $installed = (Get-ItemProperty -path Microsoft.PowerShell.Core\Registry::HKEY_LOCAL_MACHINE\Software\\Microsoft\Windows\CurrentVersion\Uninstall\* | Where DisplayName -eq $software )
 $installed.displayname
 
+#---------------------------------------------------------------------------
+#UNINSTALL APPLICATION
 if ($installed.displayName -match $software) {
-    Write-Host $software "is Installed."
+    Write-Host $software "found on this machine, and will be uninstalled."
+    #(Get-WMIObject Win32_Product -Filter name=$software).Uninstall()
 } else {
     Write-Host $software "is not installed."
 }
+
+#---------------------------------------------------------------------------
+#INSTALL APPLICATION
+if ($installed.displayName -match $software) {
+    Write-Host $software "is Installed."
+} else {
+    Write-Host $software "not found, will installed it now."
+    #Install Program
+} 
