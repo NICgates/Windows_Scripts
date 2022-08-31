b792efdc631d296a21e7ce34811af57886c5e153
NICgates <39223425+NICgates@users.noreply.github.com>
2022-08-31 14:07:10 -0700

    Update checkProgram.ps1

--- checkProgram.ps1
+++ checkProgram.ps1
@@ -1,7 +1,10 @@
-$software = "Syncro";
+
+# Set the software app to what you'd like to install or uninstall.
+$software = "APP";
 $installed = (Get-ItemProperty -path Microsoft.PowerShell.Core\Registry::HKEY_LOCAL_MACHINE\Software\\Microsoft\Windows\CurrentVersion\Uninstall\* | Where DisplayName -eq $software )
 $installed.displayname
-if ($installed.displayName -match "Syncro") {
+
+if ($installed.displayName -match $software) {
     Write-Host $software "is Installed."
 } else {
     Write-Host $software "is not installed."
