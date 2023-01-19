97372978e5dcd252dcf98a8c4607b63d6c0f6100
NICgates <39223425+NICgates@users.noreply.github.com>
2023-01-19 13:47:52 -0800

    Update Storage_Maintenance.ps1

--- Storage_Maintenance.ps1
+++ Storage_Maintenance.ps1
@@ -21,13 +21,94 @@ foreach ($userVar in $userslist) {
         Get-ChildItem -Path "C:\Users\$userVar\AppData\Roaming\Microsoft\Teams\Service Worker\CacheStorage\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
         Get-ChildItem -Path "C:\Users\$userVar\AppData\Roaming\Microsoft\Teams\Cache\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
         Get-ChildItem -Path "C:\Users\$userVar\AppData\Local\Mozilla\Firefox\Profiles\*.default-release\cache2\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+        Get-ChildItem -Path "C:\Users\$userVar\AppData\Roaming\Teams" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+
 }
+
+#------------------------------------------------------------------------
+# 3. Remove unnecessary programs, such as teams.  
+
+    $Bloatware = @(
+
+        #Unnecessary Windows 10 AppX Apps
+        "Microsoft.BingNews"
+        "Microsoft.GetHelp"
+        "Microsoft.Getstarted"
+        "Microsoft.Messaging"
+        "Microsoft.Microsoft3DViewer"
+        "Microsoft.MicrosoftOfficeHub"
+        "Microsoft.MicrosoftSolitaireCollection"
+        "Microsoft.NetworkSpeedTest"
+        "Microsoft.News"
+        "Microsoft.Office.Lens"
+        "Microsoft.Office.OneNote"
+        "Microsoft.Office.Sway"
+        "Microsoft.OneConnect"
+        "Microsoft.People"
+        "Microsoft.Print3D"
+        "Microsoft.RemoteDesktop"
+        "Microsoft.SkypeApp"
+        "Microsoft.StorePurchaseApp"
+        "Microsoft.Office.Todo.List"
+        "Microsoft.Whiteboard"
+        "Microsoft.WindowsAlarms"
+        #"Microsoft.WindowsCamera"
+        "microsoft.windowscommunicationsapps"
+        "Microsoft.WindowsFeedbackHub"
+        "Microsoft.WindowsMaps"
+        "Microsoft.WindowsSoundRecorder"
+        "Microsoft.Xbox.TCUI"
+        "Microsoft.XboxApp"
+        "Microsoft.XboxGameOverlay"
+        "Microsoft.XboxIdentityProvider"
+        "Microsoft.XboxSpeechToTextOverlay"
+        "Microsoft.ZuneMusic"
+        "Microsoft.ZuneVideo"
+
+        #Sponsored Windows 10 AppX Apps
+        #Add sponsored/featured apps to remove in the "*AppName*" format
+        "*EclipseManager*"
+        "*ActiproSoftwareLLC*"
+        "*AdobeSystemsIncorporated.AdobePhotoshopExpress*"
+        "*Duolingo-LearnLanguagesforFree*"
+        "*PandoraMediaInc*"
+        "*CandyCrush*"
+        "*BubbleWitch3Saga*"
+        "*Wunderlist*"
+        "*Flipboard*"
+        "*Twitter*"
+        "*Facebook*"
+        "*Spotify*"
+        "*Minecraft*"
+        "*Royal Revolt*"
+        "*Sway*"
+        "*Speed Test*"
+        "*Dolby*"
+             
+        #Optional: Typically not removed but you can if you need to for some reason
+        #"*Microsoft.Advertising.Xaml_10.1712.5.0_x64__8wekyb3d8bbwe*"
+        #"*Microsoft.Advertising.Xaml_10.1712.5.0_x86__8wekyb3d8bbwe*"
+        #"*Microsoft.BingWeather*"
+        #"*Microsoft.MSPaint*"
+        #"*Microsoft.MicrosoftStickyNotes*"
+        #"*Microsoft.Windows.Photos*"
+        #"*Microsoft.WindowsCalculator*"
+        #"*Microsoft.WindowsStore*"
+    )
+    foreach ($Bloat in $Bloatware) {
+        Get-AppxPackage -Name $Bloat| Remove-AppxPackage
+        Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $Bloat | Remove-AppxProvisionedPackage -Online
+        Write-Output "Trying to remove $Bloat."
+    }
+    Get-AppxPackage -Name Microsoft.Print3D | Remove-AppxPackage
 
 #------------------------------------------------------------------------
 Remove-Item C:\Windows\Temp\*.* -Force -ErrorAction SilentlyContinue
 #Remove-Item C:\Windows\Prefetch\*.* -Force -ErrorAction SilentlyContinue
 #Remove-Item "C:\Documents and Settings*\Local Settings\Temp\*.*" -Force -ErrorAction SilentlyContinue
-    
+Remove-Item C:\$Recycle.bin\*.* -Force -ErrorAction SilentlyContinue    
 Clear-RecycleBin -Force
+
+
 
 Set-ExecutionPolicy Restricted
