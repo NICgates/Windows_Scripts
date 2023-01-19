b21532d682bafcb89153a81756c8ac8ac79a5d5e
NICgates <39223425+NICgates@users.noreply.github.com>
2023-01-19 11:27:09 -0800

    Update Storage_Maintenance.ps1

--- Storage_Maintenance.ps1
+++ Storage_Maintenance.ps1
@@ -13,33 +13,15 @@ Where-Object {$_.Name -notin "Nick" -and "bcsadmin" -and "Administrator"} | Remo
 # 2. If a user has logged into computer < 3 months, then delete unnecessary temp and cached data. 
 $userslist = Get-ChildItem -Directory "C:\Users"
 foreach ($userVar in $userslist) {
-    #$userLastLogon = Get-ADUser -Identity $userVar -Properties LastLogon
-        if (Test-Path C:\Users\$userVar\AppData\Temp) {
-            Remove-Item C:\Users\$userVar\AppData\Temp\*.* -Force -ErrorAction SilentlyContinue
-        }
-        if (Test-Path "C:\Users\$userVar\AppData\Local\Google\User Data\Profile 1\Cache\") {
-            Remove-Item "C:\Users\$userVar\AppData\Local\Google\User Data\Profile 1\Cache\*.*" -Force -ErrorAction SilentlyContinue
-        }
-        if (Test-Path "C:\Users\$userVar\AppData\Local\Google\User Data\Profile 1\Code Cache\") {
-            Remove-Item "C:\Users\$vuserVar\AppData\Local\Google\User Data\Profile 1\Code Cache\*.*" -Force -ErrorAction SilentlyContinue
-        }
-        if (Test-Path "C:\Users\$userVar\AppData\Local\Google\User Data\Default\Cache\") {
-            Remove-Item "C:\Users\$userVar\AppData\Local\Google\User Data\Default\Cache\*.*" -Force -ErrorAction SilentlyContinue
-        }
-        if (Test-Path "C:\Users\$userVar\AppData\Local\Google\User Data\Default\Code Cache\") {
-            Remove-Item "C:\Users\$userVar\AppData\Local\Google\User Data\Default\Code Cache\*.*" -Force -ErrorAction SilentlyContinue
-        }
-        if (Test-Path "C:\Users\$userVar\AppData\Roaming\Microsoft\Teams\Service Worker\CacheStorage\") {
-            Remove-Item "C:\Users\$userVar\AppData\Roaming\Microsoft\Teams\Service Worker\CacheStorage\*.*" -Force -ErrorAction SilentlyContinue
-        }
-        if (Test-Path "C:\Users\$userVar\AppData\Roaming\Microsoft\Teams\Cache\") {
-            Remove-Item "C:\Users\$userVar\AppData\Roaming\Microsoft\Teams\Cache\*.*" -Force -ErrorAction SilentlyContinue
-        }
-        if (Test-Path "C:\Users\$userVar\AppData\Local\Mozilla\Firefox\Profiles\*.default-release\cache2\") {
-            Remove-Item "C:\Users\$userVar\AppData\Local\Mozilla\Firefox\Profiles\*.default-release\cache2\*.*" -Force -ErrorAction SilentlyContinue
-        }
+        Get-ChildItem -Path "C:\Users\$userVar\AppData\Temp" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+        Get-ChildItem -Path "C:\Users\$userVar\AppData\Local\Google\User Data\Profile 1\Cache\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+        Get-ChildItem -Path "C:\Users\$userVar\AppData\Local\Google\User Data\Profile 1\Code Cache\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+        Get-ChildItem -Path "C:\Users\$userVar\AppData\Local\Google\User Data\Default\Cache\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+        Get-ChildItem -Path "C:\Users\$userVar\AppData\Local\Google\User Data\Default\Code Cache\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+        Get-ChildItem -Path "C:\Users\$userVar\AppData\Roaming\Microsoft\Teams\Service Worker\CacheStorage\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+        Get-ChildItem -Path "C:\Users\$userVar\AppData\Roaming\Microsoft\Teams\Cache\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
+        Get-ChildItem -Path "C:\Users\$userVar\AppData\Local\Mozilla\Firefox\Profiles\*.default-release\cache2\" -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
 }
-
 
 #------------------------------------------------------------------------
 Remove-Item C:\Windows\Temp\*.* -Force -ErrorAction SilentlyContinue
