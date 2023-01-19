2cc0f434cc42a667e94353d30cd16a25fa39b0f5
NICgates <39223425+NICgates@users.noreply.github.com>
2023-01-19 12:51:07 -0800

    Update Storage_Maintenance.ps1

--- Storage_Maintenance.ps1
+++ Storage_Maintenance.ps1
@@ -25,6 +25,9 @@ foreach ($userVar in $userslist) {
 
 #------------------------------------------------------------------------
 Remove-Item C:\Windows\Temp\*.* -Force -ErrorAction SilentlyContinue
+Remove-Item C:\Windows\Prefetch\*.* -Force -ErrorAction SilentlyContinue
+#Remove-Item "C:\Documents and Settings*\Local Settings\Temp\*.*" -Force -ErrorAction SilentlyContinue
+    
 Clear-RecycleBin -Force
 
 Set-ExecutionPolicy Restricted
