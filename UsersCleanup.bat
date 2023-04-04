90e98a8331a4b45f477b77755e2c6d2680144e2b
NICgates <39223425+NICgates@users.noreply.github.com>
2023-04-04 15:52:55 -0700

    Update UsersCleanup.bat

--- UsersCleanup.bat
+++ UsersCleanup.bat
@@ -1,2 +1,2 @@
 ::Clean up user accounts older than 60 days
-forfiles /p "C: \Users" /d -60 /c "cmd /c (IF /i @fname NEQ “nick” IF @file NEQ “bcsadmin” (rmdir /s /q @path))” 
+forfiles /p "C:\Users" /d -60 /c "cmd /c (IF /i @fname NEQ “nick” IF @file NEQ “bcsadmin” (rmdir /s /q @path))” 
