bb0ee13e8ec45fbb15a201cc4f075f54c8fa8750
NICgates <39223425+NICgates@users.noreply.github.com>
2023-04-06 11:19:35 -0700

    Update Set_NIC_GP.bat

--- Set_NIC_GP.bat
+++ Set_NIC_GP.bat
@@ -1,11 +1,11 @@
 ::Set Wireless Networks
-netsh wlan set profileparameter name="BCS" connectionmode=Manual
-netsh wlan set hostednetwork mode=allow SSID="BCS2" Key="$chool1$Cool"
-netsh wlan set profileparameter name="BCS2" connectionmode=Auto
+netsh wlan set profileparameter name="[SSID1]" connectionmode=Manual
+netsh wlan set hostednetwork mode=allow SSID="[SSID1]" Key="[PASSWORD]"
+netsh wlan set profileparameter name="[SSID2]" connectionmode=Auto
 
 ::Set primary and secondary DNS
-netsh interface ipv4 set dns name="Wi-Fi" static 192.168.1.250
-netsh interface ipv4 add dns name="Wi-Fi" 192.168.1.251 index=2
+netsh interface ipv4 set dns name="Wi-Fi" static [PRIMARY DNS]
+netsh interface ipv4 add dns name="Wi-Fi" [SECONDARY DNS] index=2
 
 ::Enable Network Discovery 
 netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
