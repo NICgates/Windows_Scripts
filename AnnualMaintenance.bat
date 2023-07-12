2d01165c95b7d29a1cbf12fae49f1d42ac3b7a37
NICgates <39223425+NICgates@users.noreply.github.com>
2023-07-12 14:51:02 -0700

    Update AnnualMaintenance.bat

--- AnnualMaintenance.bat
+++ AnnualMaintenance.bat
@@ -1,14 +1,17 @@
 ::##############################################################################
 :: Join Networks 
-
+netsh wlan set profileparameter name="BCS" connectionmode=Manual
+netsh wlan set hostednetwork mode=allow SSID="BCS2" Key="[PASSWORD]"
+netsh wlan set profileparameter name="BCS2" connectionmode=Auto
 
 ::##############################################################################
 :: Set DNS
-
+netsh interface ipv4 set dns name="Wi-Fi" static 192.168.1.250
+netsh interface ipv4 add dns name="Wi-Fi" 192.168.1.251 index=2
 
 ::##############################################################################
 :: Turn on Network Discovery for Domain Network Profile 
-
+netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
 
 ::##############################################################################
 :: Turn on System Protection, delete old restore points, create a restore point
@@ -22,8 +25,7 @@
 ::##############################################################################
 :: Group Policy update
 gpupdate /force 
-
-
-::##############################################################################
-:: Update Windows
-
+gpresult -h C:\gpresultJuly.html
+echo "Restart computer?"
+pause
+shutdown /r
